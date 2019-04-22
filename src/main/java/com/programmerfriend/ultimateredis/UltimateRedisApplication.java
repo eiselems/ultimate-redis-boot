02c5ebecf50141bb65db6bea7a3ea4851a65f2c6
eiselems <marcus.eisele@gmail.com>
2019-04-22 08:26:23 +0200

    Add Parameters and Keys

--- src/main/java/com/programmerfriend/ultimateredis/UltimateRedisApplication.java
+++ src/main/java/com/programmerfriend/ultimateredis/UltimateRedisApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import java.util.UUID;
+
 @EnableCaching
 @SpringBootApplication
 @Slf4j
@@ -24,27 +26,34 @@ public class UltimateRedisApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        String firstString = cacheService.cacheThis();
+        String firstString = cacheService.cacheThis("param1", UUID.randomUUID().toString());
         log.info("First: {}", firstString);
-        String secondString = cacheService.cacheThis();
+        String secondString = cacheService.cacheThis("param1", UUID.randomUUID().toString());
         log.info("Second: {}", secondString);
+        String thirdString = cacheService.cacheThis("AnotherParam", UUID.randomUUID().toString());
+        log.info("Third: {}", thirdString);
+        String fourthString = cacheService.cacheThis("AnotherParam", UUID.randomUUID().toString());
+        log.info("Fourth: {}", fourthString);
 
         log.info("Starting controlled cache: -----------");
-        String controlledFirst = getFromControlledCache();
+        String controlledFirst = getFromControlledCache("first");
         log.info("Controlled First: {}", controlledFirst);
-        String controlledSecond = getFromControlledCache();
+        String controlledSecond = getFromControlledCache("second");
         log.info("Controlled Second: {}", controlledSecond);
 
-        log.info("Clearing all cache entries:");
-        cacheService.forgetAboutThis();
-        controlledCacheService.removeFromCache();
+        getFromControlledCache("first");
+        getFromControlledCache("second");
+        getFromControlledCache("third");
+        //log.info("Clearing all cache entries:");
+        //cacheService.forgetAboutThis("param1");
+        //controlledCacheService.removeFromCache("controlledParam1");
     }
 
-    private String getFromControlledCache() {
-        String fromCache = controlledCacheService.getFromCache();
+    private String getFromControlledCache(String param) {
+        String fromCache = controlledCacheService.getFromCache(param);
         if (fromCache == null) {
             log.info("Oups - Cache was empty. Going to populate it");
-            String newValue = controlledCacheService.populateCache();
+            String newValue = controlledCacheService.populateCache(param, UUID.randomUUID().toString());
             log.info("Populated Cache with: {}", newValue);
             return newValue;
         }
