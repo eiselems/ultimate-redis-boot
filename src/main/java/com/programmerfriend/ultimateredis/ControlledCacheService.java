96270658a1325e22f247b4c7caf1f7f1eb747a47
eiselems <marcus.eisele@gmail.com>
2019-04-25 21:16:01 +0200

    Add CacheKeyGenerator for ControlledCacheService

--- src/main/java/com/programmerfriend/ultimateredis/ControlledCacheService.java
+++ src/main/java/com/programmerfriend/ultimateredis/ControlledCacheService.java
@@ -8,17 +8,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class ControlledCacheService {
 
-    @Cacheable(cacheNames = "myControlledCache", key = "'myControlledPrefix_'.concat(#relevant)")
+    private static final String CONTROLLED_PREFIX = "myControlledPrefix_";
+
+    public static String getCacheKey(String relevant){
+        return CONTROLLED_PREFIX + relevant;
+    }
+
+    @Cacheable(cacheNames = "myControlledCache", key = "T(com.programmerfriend.ultimateredis.ControlledCacheService).getCacheKey(#relevant)")
     public String getFromCache(String relevant) {
         return null;
     }
 
-    @CachePut(cacheNames = "myControlledCache", key = "'myControlledPrefix_'.concat(#relevant)")
+    @CachePut(cacheNames = "myControlledCache", key = "T(com.programmerfriend.ultimateredis.ControlledCacheService).getCacheKey(#relevant)")
     public String populateCache(String relevant, String unrelevantTrackingId) {
         return "this is it again!";
     }
 
-    @CacheEvict(cacheNames = "myControlledCache", key = "'myControlledPrefix_'.concat(#relevant)")
+    @CacheEvict(cacheNames = "myControlledCache", key = "T(com.programmerfriend.ultimateredis.ControlledCacheService).getCacheKey(#relevant)")
     public void removeFromCache(String relevant) {
     }
 
