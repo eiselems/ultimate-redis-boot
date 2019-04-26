8c8b0cdc4a3d64419ac25abcaeaea898b98490fe
eiselems <marcus.eisele@gmail.com>
2019-04-26 06:44:15 +0200

    Make TTL Configurable per Cache (with default TTL)

---
+++ src/main/java/com/programmerfriend/ultimateredis/config/CacheConfig.java
@@ -0,0 +1,66 @@
+package com.programmerfriend.ultimateredis.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+@Configuration
+@EnableConfigurationProperties(CacheConfigurationProperties.class)
+@Slf4j
+public class CacheConfig extends CachingConfigurerSupport {
+
+    private static RedisCacheConfiguration createCacheConfiguration(long timeoutInSeconds) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(timeoutInSeconds));
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(CacheConfigurationProperties properties) {
+        log.info("Redis (/Lettuce) configuration enabled. With cache timeout " + properties.getTimeoutSeconds() + " seconds.");
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(properties.getRedisHost());
+        redisStandaloneConfiguration.setPort(properties.getRedisPort());
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+        redisTemplate.setConnectionFactory(cf);
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration(CacheConfigurationProperties properties) {
+        return createCacheConfiguration(properties.getTimeoutSeconds());
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory, CacheConfigurationProperties properties) {
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+
+        for (Entry<String, Long> cacheNameAndTimeout : properties.getCacheExpirations().entrySet()) {
+            cacheConfigurations.put(cacheNameAndTimeout.getKey(), createCacheConfiguration(cacheNameAndTimeout.getValue()));
+        }
+
+        return RedisCacheManager
+                .builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration(properties))
+                .withInitialCacheConfigurations(cacheConfigurations).build();
+    }
+}
