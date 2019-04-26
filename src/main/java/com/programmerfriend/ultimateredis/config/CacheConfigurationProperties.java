6a5cdc64b24d1349905a3bf12dfa8882ba08f001
eiselems <marcus.eisele@gmail.com>
2019-04-26 06:44:15 +0200

    Make TTL Configurable per Cache (with default TTL)

---
+++ src/main/java/com/programmerfriend/ultimateredis/config/CacheConfigurationProperties.java
@@ -0,0 +1,18 @@
+package com.programmerfriend.ultimateredis.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "cache")
+@Data
+public class CacheConfigurationProperties {
+
+    private long timeoutSeconds = 60;
+    private int redisPort = 6379;
+    private String redisHost = "localhost";
+    // Mapping of cacheNames to expira-after-write timeout in seconds
+    private Map<String, Long> cacheExpirations = new HashMap<>();
+}
